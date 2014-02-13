c407796daddb8d813245d780f17921d497eb5fc7
gctacker <gtacker@asu.edu>
2014-02-12 18:59:54 -0700

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+/* Hello World program */
+/*This file is from http://groups.engin.umd.umich.edu/CIS/course.des/cis400/c/hworld.html*/
+
+#include<stdio.h>
+
+main()
+{
+    printf("Hello World");
+
+}
