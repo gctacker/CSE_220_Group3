e3f7a54dd1b76b6ce3d90f8a594346642a2d1dd3
gctacker <gtacker@asu.edu>
2014-02-23 21:58:26 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main (int argc, const char *argv[])
 {
 	
-    FILE *source_file;
+    FILE *source_file = NULL;
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
 
@@ -16,40 +16,32 @@ int main (int argc, const char *argv[])
 	  printf("you didnt provide an argument\n");
 	  return 1;
 	}
-	if(argc > 2)
-	{
-	  printf("you provided too many arguments\n");
-	  return 1;
-	}
 	
+	init_lister(argv[1], source_name, date);
+	source_file =  fopen(argv[1], "r");
+	 
+
+
 	
-	*source_file = *init_lister( argv[1], source_name, date);
-
-
-
-	get_source_line = TRUE;
-
-
-	while(get_source_line)
+	while(get_source_line(source_file, source_name, date))
 	{
-		get_source_line(source_file, source_name, date);
+		
 	}
 	
     fclose(source_file);
-   /* xxMissing Code Here */
-    return 0;
+     return 0;
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
 {
     time_t timer;
-    FILE *file;
+    FILE *file = NULL;
 
-
+	strcpy(source_file_name, name);
+	file = fopen("source_file_name", "r");
     	timer = time(NULL);
-	sprintf(&dte[0], "%s", ctime(&timer)); 
-	file = fopen(name, "r");    
-
-    return file;
+	sprintf(dte, "%s", ctime(&timer));
+	 
+	return file;
 }
 
 
@@ -61,15 +53,11 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
     static int line_number = 0;    
 
-
-    fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file);
-    printf("%s \n", source_buffer);
-
-    if ( source_buffer != NULL) 
-    {
+    if (fgets(source_buffer,MAX_SOURCE_LINE_LENGTH,src_file)) 
+    { 
 	line_number++;
 	sprintf(print_buffer, "%d %s", line_number, source_buffer);
-
+	
 	print_line(print_buffer, src_name, todays_date);
 		/*  xxMissing Code Here */
 
