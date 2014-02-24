a1da449068543ab364f4c9a2098707af400cb4d9
gctacker <gtacker@asu.edu>
2014-02-23 18:19:10 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,20 +6,21 @@ int main (int argc, const char *argv[])
     FILE *source_file;
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
+char source_buff[MAX_SOURCE_LINE_LENGTH];
 	
+
 	if(argc < 2)
 	{
 	  printf("you didnt provide an argument\n");
 	  return 1;
+	}
+	if(argc > 2)
+	{
+	  printf("you provided too many arguments\n");
+	  return 1;
 	}	
-
-	/*sprintf(&source_name[0], "%s", argv[1]);*/
-	/*source_name[0]= argv[1];*/
-
 	
-
-	*source_file = *init_lister(argv[1], source_name, date);
-	
+*source_file = *init_lister(argv[1], source_name, date);
 	while(get_source_line(source_file, source_name, date))
 	{
 		
@@ -38,34 +39,31 @@ FILE *init_lister(const char *name, char source_file_name[], char dte[])
     	timer = time(NULL);
 	sprintf(&dte[0], "%s", ctime(&timer)); 
 	file = fopen(name, "r");
-	
-	
     
-    /* xxMissing Code Here */
+
     return file;
 }
+
+
+
 BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
 {
-	if(TRUE)
-	{
-	  printf("%s\n", todays_date);
-	}
 
     char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
     static int line_number = 0;    
 
+
+    fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file);
+    if ( source_buffer != NULL) 
+    {
+	line_number++;
+	sprintf(print_buffer, "%d %s", line_number, source_buffer);
+
 if(TRUE)
 	{
-	  printf("%d\n", line_number);
-	}	
-
-    if (fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file) != NULL) 
-    {
-
-
-	line_number++;
-	sprintf(print_buffer, "%d %s", line_number, &source_buffer[0]);
+	  printf("%s\n", print_buffer);
+	}
 	print_line(print_buffer, src_name, todays_date);
 		/*  xxMissing Code Here */
 
